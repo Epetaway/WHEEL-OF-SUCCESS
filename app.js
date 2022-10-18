9a76be2e320afaf64568afeaef5256a741a1a2bf
Earl <earlpetaway@gmail.com>
2022-10-18 09:51:01 -0400

    init game app

---
+++ app.js
@@ -0,0 +1,92 @@
+const phraseList = ["The best of both worlds","Speak of the devil","See eye to eye","Once in a blue moon","When pigs fly", "To cost an arm and a leg","A piece of cake","Let the cat out of the bag","To feel under the weather","To kill two birds with one stone"];
+
+let phrase = "";
+let lives = "";
+let key = "";
+let phraseSplit = "";
+
+$( document ).ready(function() {
+$('#overlay.lose, #overlay.win').hide();
+});
+
+
+$('#start-btn, #restart-btn-win, #restart-btn-lose').click((event) => { // Listens for click functionon start button and calls getPhrase and displays the phrase
+  lives = 5;
+  console.log('click');
+  $('ul').empty();
+  $('button.chosen').removeClass('chosen');
+  $('#overlay.lose, #overlay.win, #overlay.start').hide();
+  $('#banner, #phrase, #qwerty, #scoreboard').css('display','initial');
+  $('li.tries img').attr('src', 'images/liveHeart.png');
+
+  let phraseSplit = getPhrase();
+  key = phrase.replace(/ /g,'');
+  string = phraseSplit;
+  stringArray = new Array();
+    for(var i =0; i < string.length; i++){
+        stringArray.push(string[i]);
+    if(string[i] == " "){
+      $('#phrase ul').append('<li class="space"></li>');
+    } else {
+      // stringArray.push(" ");
+      $('#phrase ul').append('<li class="letter ' + [i] + '">*</li>');
+      // $("'letter '" + [i] + "'").val("'" + string[i] + "'")
+
+    }
+  }
+  console.log(phraseSplit);
+});
+
+
+
+
+
+
+
+
+let getPhrase = () => { //Gets a randomphrase from phrase list
+  phrase = phraseList[Math.floor((Math.random() * 9) + 1)];
+  console.log(Math.floor((Math.random() * 9) + 1));
+  let phraseSplit = phrase.split('');
+  return phraseSplit;
+
+}
+
+$('#qwerty button').click(function(event) {
+  checkLetter($(this).text());
+  $(this).addClass('chosen');
+   if (lives == 0){
+    $('.lose').show();
+    $('button').removeClass('chosen');
+  }
+
+});
+
+    // compare letter to phrase
+
+
+
+let checkLetter = (letter) => { //Check if letter is in phrase and show on board
+  const correct = [];
+  let idx = stringArray.map(stringArray => stringArray.toLowerCase());
+    idx = idx.indexOf(letter);
+    if (idx === -1) {
+      console.log('wrong');
+      let tries = $('li.tries img');
+      let arrayNum = lives -1;
+  // console.log(tries[lives - 1]);
+      $(tries[arrayNum]).attr('src', 'images/lostHeart.png');
+      lives = lives - 1;
+    }
+    while (idx !== -1) {
+      correct.push(idx);
+      for(var i =0; i < correct.length; i++){
+        let selector = "." + correct[i];
+
+        $(selector).addClass('show animate__animated animate__tada').html(letter);
+
+      }idx = stringArray.indexOf(letter, idx + 1);
+
+    }
+
+}
